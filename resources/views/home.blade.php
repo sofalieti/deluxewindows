cc5fe9506ba2f9cefa3dd3b3f2748d3f8280f1ff
sofalieti <Sofalieti@gmail.com>
2026-05-26 12:37:12 +0300

    пар

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <!-- This site was created in Webflow. https://webflow.com --><!-- Last Published: Tue May 26 2026 07:52:04 GMT+0000 (Coordinated Universal Time) -->
 <html
   data-wf-domain="www.deluxewindows.com"
@@ -1288,18 +1288,8 @@
   </div>
 
   <div id="menuDimmer" style="opacity: 0; pointer-events: none"></div>
-  <script
-    src="/webflow-assets/js/jquery-3.5.1.min.js"
-    type="text/javascript"
-    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-    crossorigin="anonymous"
-  ></script>
-  <script
-    src="/webflow-assets/js/webflow.js"
-    type="text/javascript"
-    integrity="sha384-FX7Sq0B7i+W2Hvo9lm7V4bcNf0e38phq6FEOSiEWOI3HaqcMAjZJSs52EEmLyHo7"
-    crossorigin="anonymous"
-  ></script>
+  <script src="/webflow-assets/js/jquery-3.5.1.min.js" type="text/javascript"></script>
+  <script src="/webflow-assets/js/webflow.js" type="text/javascript"></script>
 
   {{-- SCROLL STYLE --}}
 </body>
