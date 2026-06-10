27e497f27dee3e0aba67aee397e8e0367b247428
sofalieti <Sofalieti@gmail.com>
2026-06-10 15:54:36 +0300

    DSF

--- resources/views/partials/service-area-faq.blade.php
+++ resources/views/partials/service-area-faq.blade.php
@@ -1,4 +1,14 @@
     @if(count($faqs) > 0)
+    @php
+      // Webflow ix2 interaction targets for service-area template (data-wf-page 69ce7898d019bc268b4bb9e4)
+      $faqAccordionWids = [
+        '7c2f1aab-0a7c-7750-8c1c-d24e5259cc04',
+        '7c2f1aab-0a7c-7750-8c1c-d24e5259cc14',
+        '7c2f1aab-0a7c-7750-8c1c-d24e5259cc22',
+        '7c2f1aab-0a7c-7750-8c1c-d24e5259cc32',
+        '7c2f1aab-0a7c-7750-8c1c-d24e5259cc02',
+      ];
+    @endphp
     <section class="section">
       <div class="w-layout-blockcontainer container-default w-container">
         <div class="inner-container _670px center">
@@ -19,18 +29,30 @@
             <div class="card accordion-card-v1">
               <div class="w-layout-grid grid-1-column accordion-v6">
                 @foreach($faqs as $index => $faq)
-                <div class="accordion-wrapper">
+                @php
+                  $wId = $faqAccordionWids[$index] ?? null;
+                @endphp
+                <div
+                  @if($wId)
+                  id="w-node-_{{ str_replace('-', '_', $wId) }}-8b4bb9e4"
+                  data-w-id="{{ $wId }}"
+                  @endif
+                  class="accordion-wrapper{{ $wId ? '' : ' js-faq-accordion' }}"
+                >
                   <div class="accordion-item-wrapper {{ $index === 0 ? 'first' : '' }}{{ $index === count($faqs) - 1 ? ' last' : '' }}">
                     <div class="accordion-top">
                       <div class="text-titles">
                         <h3 class="faqs-title">{{ $faq['question'] }}</h3>
                       </div>
-                      <div class="accordion-icon-wrapper">
-                        <div class="accordion-icon-line vertical"></div>
-                        <div class="accordion-icon-line"></div>
+                      <div class="accordion-icon-wrapper" style="background-color: rgba(0, 0, 0, 0)">
+                        <div
+                          class="accordion-icon-line vertical"
+                          style="background-color: rgb(20, 22, 28); transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(90deg) skew(0deg, 0deg); transform-style: preserve-3d;"
+                        ></div>
+                        <div class="accordion-icon-line" style="background-color: rgb(20, 22, 28)"></div>
                       </div>
                     </div>
-                    <div class="accordion-bottom v1">
+                    <div class="accordion-bottom v1" style="height: 0px">
                       <div class="w-richtext">
                         {!! $faq['answer'] !!}
                       </div>
@@ -44,4 +66,25 @@
         </div>
       </div>
     </section>
+    <script>
+    (function () {
+      document.querySelectorAll('.js-faq-accordion').forEach(function (wrapper) {
+        var top = wrapper.querySelector('.accordion-top');
+        var bottom = wrapper.querySelector('.accordion-bottom');
+        var vertical = wrapper.querySelector('.accordion-icon-line.vertical');
+        if (!top || !bottom) return;
+
+        top.style.cursor = 'pointer';
+        top.addEventListener('click', function () {
+          var open = wrapper.classList.toggle('is-open');
+          bottom.style.height = open ? bottom.scrollHeight + 'px' : '0px';
+          if (vertical) {
+            vertical.style.transform = open
+              ? 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)'
+              : 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(90deg) skew(0deg, 0deg)';
+          }
+        });
+      });
+    })();
+    </script>
     @endif
