a3566dbe0c4213dd815e8792268ff7da7d8a880e
sofalieti <Sofalieti@gmail.com>
2026-06-16 16:32:35 +0300

    fdg

--- app/Orchid/Screens/Webflow/WebflowCollectionEditScreen.php
+++ app/Orchid/Screens/Webflow/WebflowCollectionEditScreen.php
@@ -118,7 +118,7 @@ class WebflowCollectionEditScreen extends Screen
 
                 Switcher::make('entity.is_draft')
                     ->title('Draft'),
-            ]);
+            ]),
         ];
 
         if ($this->collectionSlug === 'global-settings') {
