83ec053610eb4db928a20f60f62798d91936a07c
sofalieti <Sofalieti@gmail.com>
2026-07-20 17:36:30 +0300

    dfsg

--- resources/views/layouts/classic.blade.php
+++ resources/views/layouts/classic.blade.php
@@ -33,6 +33,7 @@
     </script>
     @include('partials.seo-head')
     <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta name="theme-color" content="#0f4d89" />
     <link href="{{ site_css_bundle_url([
       'webflow-assets/css/webflow.min.css',
       'webflow-assets/css/fonts.css',
